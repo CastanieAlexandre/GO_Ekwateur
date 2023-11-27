4d520aba4b19b2e58b50c2e278414b408419cc70
CastanieAlexandre <castanie.alexandre@hotmail.com>
2023-11-27 09:42:11 +0100

    add password

--- app_go.py
+++ app_go.py
@@ -4,9 +4,39 @@ import plotly.express as px
 import warnings
 
 # Titres et configuration de la page
+import hmac
+import streamlit as st
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the passward is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 st.set_page_config(layout="wide",
-                   page_title="GO Ekwateur",
+                   page_title="Garanties d'origine",
                    page_icon=":bar_chart:")
 
 HIDE_FORMAT = """
@@ -30,7 +60,7 @@ st.markdown(
     )
 
 st.markdown(
-    "<h1 style='text-align: center;'>Garanties d'Origine Ekwateur</h1>",
+    "<h1 style='text-align: center;'>Garanties d'Origine</h1>",
     unsafe_allow_html=True
     )
 st.sidebar.write("Données à upload :")
@@ -71,17 +101,16 @@ def recup_et_mise_en_forme(chemin_csv,chemin_pp):
     return df_data
 
 def liste_selections(l_offr,l_stat):
+    l_offres_selectionnees,l_statuts_selectionnes=[],[]
 
-        l_offres_selectionnees,l_statuts_selectionnes=[],[]
+    for offer in l_offr:
+        if st.session_state[offer]:
+            l_offres_selectionnees.append(offer)
+    for stat in l_stat:
+        if st.session_state[stat]:
+            l_statuts_selectionnes.append(stat)
 
-        for offer in l_offr:
-            if st.session_state[offer]:
-                l_offres_selectionnees.append(offer)
-        for stat in l_stat:
-            if st.session_state[stat]:
-                l_statuts_selectionnes.append(stat)
-
-        return l_offres_selectionnees,l_statuts_selectionnes
+    return l_offres_selectionnees,l_statuts_selectionnes
 
 @st.cache_data(show_spinner=True)
 def selection_donnees(df,years:tuple,list_checked_offers:list,list_checked_status:list):
